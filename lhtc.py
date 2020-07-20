39bb3dee83cef2c74a5f57da8818ea4164f8016e
Lintushka <serafimalintu@gmail.com>
2020-07-20 23:26:09 +0300

    Update lhtc.py

--- lhtc.py
+++ lhtc.py
@@ -1,11 +1,23 @@
 def add(x, y):
     return x + y
 
-
+name = "Kate"
 print(add(5, 10))
 print(add("abc", "lol"))
 
 if name == 'Alice':
     print('hello, Alice')
 
-name = karla
+
+class Vehicle(object):
+
+    def __init__(self, color, doors, tries):
+        self.color = color
+        self.doors = doors
+        self.tires = tries
+
+    def brake(self):
+        return 'Braking'
+
+    def drive(self):
+        return "I'm driving!"
